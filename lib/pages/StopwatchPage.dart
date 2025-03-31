dd6b7c999c6e034bad5ee0210b5799fbf0b07329
David Comer <David.Comer@basx.com>
2025-03-31 08:31:12 -0700

    Handle hour display on stopwatch.

--- lib/pages/StopwatchPage.dart
+++ lib/pages/StopwatchPage.dart
@@ -61,10 +61,11 @@ class _StopwatchPageState extends State<StopwatchPage> {
   }
 
   String _formatTime(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-    return "$twoDigitMinutes:$twoDigitSeconds";
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  String twoDigitHours = twoDigits(duration.inHours);
+  String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+  String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+  return "$twoDigitHours:$twoDigitMinutes:$twoDigitSeconds";
   }
 
   @override
